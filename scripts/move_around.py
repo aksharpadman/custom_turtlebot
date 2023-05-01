bbee3b016dae52ab739e5b80576af3345de5b2d5
aksharpadman <aksharpadman@gmail.com>
2023-05-01 12:40:46 +0530

    fixed bugs: scripys/move_around.py

--- scripts/move_around.py
+++ scripts/move_around.py
@@ -1,21 +1,173 @@
 #!/usr/bin/env python
 import rospy
+import threading
 from  geometry_msgs.msg import Twist
 
-def main():
-    cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    rospy.init_node('move_around', anonymous=True)
-    rate=rospy.Rate(10)
-    while not rospy.is_shutdown():
-        twist = Twist()        
-        twist.linear.x = 1
+
+import sys
+from select import select
+
+if sys.platform == 'win32':
+    import msvcrt
+else:
+    import termios
+    import tty
+
+#move bindings
+moveBindings = {
+        'i':(1,0,0,0),
+        'o':(1,0,0,-1),
+        'j':(0,0,0,1),
+        'l':(0,0,0,-1),
+        'u':(1,0,0,1),
+        ',':(-1,0,0,0),
+        '.':(-1,0,0,1),
+        'm':(-1,0,0,-1),    
+    }
+
+class PublishThread(threading.Thread):
+    def __init__(self, rate):
+        super(PublishThread, self).__init__()
+        self.publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.th = 0.0
+        self.speed = 0.2
+        self.turn = 0.5
+        self.condition = threading.Condition()
+        self.done = False
+
+        # Set timeout to None if rate is 0 (causes new_message to wait forever
+        # for new data to publish)
+        if rate != 0.0:
+            self.timeout = 1.0/rate
+        else:
+            self.timeout = None
+        #start thread
+        self.start()
+
+    def wait_for_subscribers(self):
+        i = 0
+        while not rospy.is_shutdown() and self.publisher.get_num_connections() == 0:
+            if i == 4:
+                print("Waiting for subscriber to connect to {}".format(self.publisher.name))
+            rospy.sleep(0.5)
+            i += 1
+            i = i % 5
+        if rospy.is_shutdown():
+            raise Exception("Got shutdown request before subscribers connected")
+
+    def update(self, x, y, z, th):
+        self.condition.acquire()
+        self.x = x*self.speed
+        self.y = y*self.speed
+        self.z = z*self.speed
+        self.th = th*self.turn
+        # Notify publish thread that we have a new message.
+        self.condition.notify()
+        self.condition.release()
+
+    def stop(self):
+        self.done = True
+        self.update(0, 0, 0, 0)
+        self.join()
+
+    def run(self):
+        twist = Twist()
+        while not self.done:
+            self.condition.acquire()
+            # Wait for a new message or timeout.
+            self.condition.wait(self.timeout)
+
+            # Copy state into twist message.
+            twist.linear.x   = self.x 
+            twist.linear.y = self.y 
+            twist.linear.z = self.z 
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = self.th 
+
+            self.condition.release()
+
+            # Publish.
+            self.publisher.publish(twist)
+
+        # Publish stop message when thread exits.
+        twist.linear.x = 0
         twist.linear.y = 0
         twist.linear.z = 0
         twist.angular.x = 0
         twist.angular.y = 0
         twist.angular.z = 0
-        cmd_vel_publisher.publish((twist))
-        rate.sleep()
+        self.publisher.publish(twist)
+
+
+''' Functions to capture key board state'''
+def getKey(settings, timeout):
+    if sys.platform == 'win32':
+        # getwch() returns a string on Windows
+        key = msvcrt.getwch()
+    else:
+        tty.setraw(sys.stdin.fileno())
+        # sys.stdin.read() returns a string on Linux
+        rlist, _, _ = select([sys.stdin], [], [], timeout)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+def saveTerminalSettings():
+    if sys.platform == 'win32':
+        return None
+    return termios.tcgetattr(sys.stdin)
+
+def restoreTerminalSettings(old_settings):
+    if sys.platform == 'win32':
+        return
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+def main():
+    settings = saveTerminalSettings()
+    cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('move_around', anonymous=True)
+    rate=10
+    publish_thread = PublishThread(rate)
+    x = 0
+    y = 0
+    z = 0
+    th =0
+    try:
+        publish_thread.wait_for_subscribers()
+        while(1):
+            key = getKey(settings, 0.5)
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            else:
+                # Skip updating cmd_vel if key timeout and robot already
+                # stopped.
+                if key == '' and x == 0 and y == 0 and z == 0 and th == 0:
+                    continue
+                x = 0
+                y = 0
+                z = 0
+                th = 0
+                if (key == '\x03'):
+                    break
+            publish_thread.update(x, y, z, th)
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        publish_thread.stop()
+        restoreTerminalSettings(settings)
+ 
 
 
 if __name__ == '__main__':
